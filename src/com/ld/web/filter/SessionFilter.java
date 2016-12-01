4c54a1f95bd3eb10bf3a38ffc27ba51e79da4746
LD <991988207@qq.com>
2016-12-01 10:37:23 +0800

    update

--- src/com/ld/web/filter/SessionFilter.java
+++ src/com/ld/web/filter/SessionFilter.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ld.web.controller.BaseController;
-import com.ld.web.controller.UserController;
+import com.ld.web.controller.LoginController;
 import com.ld.web.util.StringUtil;
 
 /**
@@ -53,7 +53,7 @@ public class SessionFilter extends BaseController implements HandlerInterceptor 
                 resp.sendError(518, "session timeout.");
                 return false;
             }
-            resp.sendRedirect(req.getContextPath() + UserController.REQUEST_INDEX_URL);
+            resp.sendRedirect(req.getContextPath() + LoginController.REQUEST_INDEX_URL);
             return false;
         }
 
